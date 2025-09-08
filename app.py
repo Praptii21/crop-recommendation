34f343da8315b577a9a3e6cc35fe52c38db7ad1e
Prapti <praptiyush2007@gmail.com>
2025-09-09 02:43:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from database import db 
+from routes.crop import crop_bp
+from routes.disease import disease_bp
+from routes.ml_crop import ml_crop_bp 
+from routes.data import data_bp
+
+app = Flask(__name__)
+
+# 🔹 SQLite database config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///agroassist.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# 🔹 initialize DB
+db.init_app(app)
+
+app.register_blueprint(crop_bp)
+app.register_blueprint(disease_bp)
+app.register_blueprint(data_bp)
+app.register_blueprint(ml_crop_bp)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "AgroAssist Backend Running 🚀"})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # create tables if not already
+    app.run(debug=True)
